f26b9bc4030ad63c344027f86741d0c3cfee5eec
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2024-06-25 13:26:21 +0100

    Update layout.tsx

--- app/(root)/profile/layout.tsx
+++ app/(root)/profile/layout.tsx
@@ -4,7 +4,7 @@ import NavBar from "./nav-bar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="sticky top-02>
+      <div className="sticky top-0">
       <Header title="My profile" nav={<NavBar />} />
       </div>
       {children}
