a7b87cda329daed83ce55995ad883aabf452b3f7
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2024-06-25 13:14:36 +0100

    Update header.tsx

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,7 +4,7 @@ const Header = ({ title, nav }: { title: string; nav: React.ReactNode }) => {
   return (
     
     <div className="">
-      <div className="header ">
+      <div className="header sticky top-0 ">
         <div>{title}</div>
         <div className="header-right">
           <ModeToggle />
