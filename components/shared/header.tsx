f8f3acea87b1be63f6fafe5522417219c7e63316
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2024-06-25 13:04:35 +0100

    Update header.tsx

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,9 +2,9 @@ import ModeToggle from "./mode-toogle";
 
 const Header = ({ title, nav }: { title: string; nav: React.ReactNode }) => {
   return (
-    <div className="sticky top-0">
     <div className="">
-      <div className="header">
+    <div className="">
+      <div className="header sticky top-0">
         <div>{title}</div>
         <div className="header-right">
           <ModeToggle />
