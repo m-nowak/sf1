9f184ad497cf14c9d471ee6718f6bb6c28f7664c
Michal <cwiarinio@googlemail.com>
2024-06-28 17:50:13 +0100

    6c

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/providers/theme-provider";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import queryClient from "../lib/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
--- components/shared/time.tsx
+++ components/shared/time.tsx
@@ -5,22 +5,12 @@ import { useState, useEffect } from "react";
 const Time = () => {
   const [time, setTime] = useState<string | null>(null);
   const { data, error, isLoading } = useTime();
-  useEffect(() => {
-    async function fetchTime() {
-      const res = await fetch("https://sf1.vercel.app/api/time", {
-        cache: "no-store",
-      });
-      const data = await res.json();
-      setTime(data.currentTime);
-    }
 
-    fetchTime();
-  }, []);
   return (
     <div>
       {" "}
-      {time ? (
-        <p className="mt-4 text-lg">{time}</p>
+      {data ? (
+        <p className="mt-4 text-lg">{data.currentTime}</p>
       ) : (
         <p className="mt-4 text-lg">Loading...</p>
       )}
