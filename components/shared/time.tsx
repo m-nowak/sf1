17b6d119f0dd6f110f6a1d5327e9fc34dd744cfa
Michal <cwiarinio@googlemail.com>
2024-06-28 18:19:10 +0100

    6c

--- components/shared/time.tsx
+++ components/shared/time.tsx
@@ -1,16 +1,13 @@
-"use client";
 import { useTime } from "@/hooks/use-time";
-import { useState, useEffect } from "react";
 
 const Time = () => {
-  const [time, setTime] = useState<string | null>(null);
   const { data, error, isLoading } = useTime();
 
   return (
     <div>
       {" "}
       {data ? (
-        <p className="mt-4 text-lg">{data.currentTime}</p>
+        <p className="mt-4 text-lg">{data.currentTime} 1</p>
       ) : (
         <p className="mt-4 text-lg">Loading...</p>
       )}
