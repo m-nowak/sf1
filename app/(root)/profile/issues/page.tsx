b1bc2545f7a343a64633f565856559ec1cfda935
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2024-07-01 12:03:30 +0100

    Update page.tsx

--- app/(root)/profile/issues/page.tsx
+++ app/(root)/profile/issues/page.tsx
@@ -23,17 +23,7 @@ import { useQuery } from '@tanstack/react-query';
   const IssuesPage = async () => {
     const thisTime = await fetchTime();
 
-      const { data, error, isLoading } = useQuery({
-    queryKey: ['time'],
-    queryFn: async () => {
-      const response = await fetch('https://sf1.vercel.app/api/time');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    },
-    refetchInterval: 5000, // Refetch every 5 seconds
-  });
+ 
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
