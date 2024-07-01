1fdc338977b66734f74aade4b703c7f8b94758de
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2024-07-01 10:00:42 +0100

    Update time.tsx

--- components/shared/time.tsx
+++ components/shared/time.tsx
@@ -2,19 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchTime } from '@/hooks/use-time';
 
 const Time = () => {
-  const { data, error, isLoading } = useQuery<any, Error>({
-    queryKey: ['time'],
-    queryFn: fetchTime,
-    refetchInterval: 5000, // Refetch every 5 seconds
-  });
+  // const { data, error, isLoading } = useQuery<any, Error>({
+  //   queryKey: ['time'],
+  //   queryFn: fetchTime,
+  //   refetchInterval: 5000, // Refetch every 5 seconds
+  // });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
       <h1>Current Time</h1>
-      <p>{data}</p>
+{/*       <p>{data}</p> */}
     </div>
   );
 };
